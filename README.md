fa00479dd46d28fd88f65395f31a7675c6795d3e
chrishester <cjhester23@gmail.com>
2024-05-27 09:50:13 -0700

    Added more logic for analyzing a given stock

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 ##Stock Watcher### 
+
+Flow Chart of program
+
+#1. - Get yesterday's closing stock price. I used list comprehensions on Python dictionaries. e.g. [new_value for (key, value) in dictionary.items()]
+
+#2. - Get the day before yesterday's closing stock price
+
+#3. - Find the positive difference between 1 (yesterday) and 2 (day before yesterday).
+
+#4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
+
+#5. -  TEST --> If percent difference is greater than 5 then print("Get News").
+
+    ## STEP 2: https://newsapi.org/ 
+    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+
+#6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
+
+#7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
+
+
+    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
+    #to send a separate message with each article's title and description to your phone number. 
+
+#8. - Create a new list of the first 3 article's headline and description using list comprehension.
+
+#9. - Send each article as a separate message via Twilio. 
--- main.py
+++ main.py
@@ -2,19 +2,58 @@
 
 import requests
 import creds
-#Stock to extract data from
 
+#Stock name to extract data from
 STOCK_NAME = 'TSLA'
 COMPANY_NAME = "TESLA INC"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
+#API params for time series daily
 stock_params = {
     "function": "TIME_SERIES_DAILY",
     "symbol": STOCK_NAME,
     "apikey": creds.STOCK_API_KEY
 }
-
+#testing API response
 response = requests.get(STOCK_ENDPOINT, params=stock_params)
-print(response.json())
+data = (response.json()["Time Series (Daily)"])
+"""
+date is the key  in 'Time series (daily)', we need a way to be able to iterate to the next
+key when we fetch tomorrow's data. We can do this using list comprehension
+"""
+data_list = [value for (key,value) in data.items()]
+
+#print(data_list)
+
+#Yesterdays data will always be in the 0 index
+yesterdays_data = data_list[0]
+
+#Yesterday's closing price
+yesterdays_closing_price = yesterdays_data["4. close"]
+
+#print(yesterdays_closing_price)
+
+
+#Day before yesterday's closing price
+day_before_yesterdays_data = data_list[1]
+#Day before yesterday's closing price
+day_before_yesterdays_closing_price = day_before_yesterdays_data["4. close"]
+
+#print(day_before_yesterdays_closing_price)
+
+
+#Find the positive difference between yesterday and day before yesterday
+close_difference = abs(float(day_before_yesterdays_closing_price)-float(yesterdays_closing_price))
+#print(close_difference)
+
+#percentage difference in price between closing price yesterday and closing price the day before yesterday.
+diff_percent = (close_difference/float(yesterdays_closing_price)) *100
+
+#print(diff_percent)
+
+#if percentage is greater than 5 then print("Get News").
+
+if diff_percent > 3:
+    print("Get News")
